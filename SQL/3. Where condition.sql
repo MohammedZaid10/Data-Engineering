526422a364a327d5a8370e6e8b7d5345a5cf23ae
Mohamed Zaid <64163993+MohammedZaid10@users.noreply.github.com>
2024-05-21 05:48:26 +0530

    Update 3. Where condition.sql

--- SQL/3. Where condition.sql
+++ SQL/3. Where condition.sql
@@ -1,5 +1,8 @@
 -- SYNTAX
 SELECT * FROM table WHERE condition;
 
--- EXAMPLE
+-- EXAMPLE 1
 SELECT name,choice FROM table WHERE name = 'DAVID';
+
+-- EXAMPLE 2
+SELECT COUNT(*) FROM Film WHERE rentalrate >=4 AND replacement.cost >= 19.99 AND rating!='R';
