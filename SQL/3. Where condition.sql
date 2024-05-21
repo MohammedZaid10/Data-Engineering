4b505b580207d5b0028ca8018f184b6e9ebd15df
Mohamed Zaid <64163993+MohammedZaid10@users.noreply.github.com>
2024-05-21 06:47:38 +0530

    Delete SQL/3. Where condition.sql

--- SQL/3. Where condition.sql
+++
@@ -1,8 +0,0 @@
--- SYNTAX
-SELECT * FROM table WHERE condition;
-
--- EXAMPLE 1
-SELECT name,choice FROM table WHERE name = 'DAVID';
-
--- EXAMPLE 2
-SELECT COUNT(*) FROM Film WHERE rentalrate >=4 AND replacement.cost >= 19.99 AND rating!='R';
